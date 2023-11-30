9dff74b2fcd56bc1ea88da62cc24e736bffc75f4
helpmehelpus <adrianortorres@gmail.com>
2023-11-30 18:43:19 +1030

    introduce basic server and client errors

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ pub use self::error::{Error, Result};
 
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::{middleware, Router};
+use axum::{Json, middleware, Router};
 use std::net::SocketAddr;
 use axum::extract::{Path, Query};
 use serde::Deserialize;
+use serde_json::json;
 use tower_http::services::ServeDir;
 use tower_cookies::CookieManagerLayer;
+use uuid::Uuid;
 use crate::model::ModelController;
 
 mod ctx;
@@ -48,8 +50,30 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("->> client_error_body: {client_error_body}");
+
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    println!("->> server log line - {uuid} - Error: {service_error:?}");
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
